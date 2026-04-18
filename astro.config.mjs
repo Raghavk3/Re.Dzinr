9ff57e37bf249c385a8bf252d625d9a22900d9ac
Raghava Kesanapalli <raghavvarshith@gmail.com>
2026-04-18 20:41:05 +1000

    Recommitting tedx changes(4)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
